de8a14dda407dda56e0b42b25f8ab6210ebf40be
Soufiane Ammy Driss <=>
2021-01-04 17:25:13 +0100

    end!

--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function getMessage() {
+    return "Muy bien!";
+}
+const Ok = () => {
+    const btnText = "Good";
+    const btnSTyle = {backgroundColor: 'blue', color: 'white'};
+    const pText = {text: 'Allez!'};
+    return (
+        <div>
+            <h1>Hello react world!</h1>
+            <h4>{getMessage()}</h4>
+            <p>{pText.text}</p>
+            <label htmlFor="test">Test</label>
+            <input type="text" name="test" id="test" />
+            <button style={btnSTyle}>
+                {btnText}
+            </button>
+        </div>
+    );
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Ok/>,
   </React.StrictMode>,
   document.getElementById('root')
 );
